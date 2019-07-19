1faaea6fad11a2601da4bc811cbaff2fb5938879
alekseyVY <alekseyvy@gmail.com>
2019-07-19 12:31:34 +0400

    v 0.5 part_1 (Exercises 1.1, 1.2)

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,26 +1,65 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+
+const Header = (props) => {
     return (
         <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <div>
+            <h3>
+                {props.p} {props.e}
+            </h3>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <>
+        <Part p = {props.pt1} e = {props.exec1} />
+        <Part p = {props.pt2} e = {props.exec2} />
+        <Part p = {props.pt3} e = {props.exec3} />
+        </>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <div>
+            <h3>
+                Number of exercises: {props.exe1 + props.exe2 + props.exe3}
+            </h3>
         </div>
     )
 }
 
 
+
 const App = () => {
-    const name = 'Peter'
-    const age = 10
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
     return (
         <>
-            <h1>Greetings</h1>
-            <Hello name="Aleksey" age={26 + 10}/>
-            <Hello name={name} age={age}/>
+            <Header course = {course}/>
+            <Content pt1 = {part1} exec1 = {exercises1}
+            pt2 = {part2} exec2 = {exercises2}
+            pt3 = {part3} exec3 = {exercises3}/>
+            <Total exe1 = {exercises1} exe2 = {exercises2} exe3 = {exercises3}/>
         </>
-)
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
