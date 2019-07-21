0dbc6fa592b172a1f63bd039527306b63a70517f
alekseyVY <alekseyvy@gmail.com>
2019-07-21 20:54:28 +0400

    v 0.9 part_1 (Exercises 1.8) unicafe part 3 (refractored so that statistics now in separate component)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,18 +10,39 @@ const Button = (props) => (
 
 const Display = (props) => <span>{props.value}</span>
 
+const Statistics = (value) => {
+
+    //variable that counts total feedback
+    let all = value.g + value.n + value.b
+    //variable that counts average score (good: 1, neutral: 0, bad: -1)
+    let average = (value.g - value.b) / all
+    //variable that calculates positive percentage from all feedback
+    let positivePercent = (((all - (value.n + value.b)) / all) * 100)
+
+    return (
+        <div>
+            <h1>statistics</h1>
+            <p>good <Display value={value.g}/></p>
+            <p>neutral <Display value={value.n}/></p>
+            <p>bad <Display value={value.b}/></p>
+            <p>all <Display value={all}/></p>
+            <p>average <Display value={average}/></p>
+            <p>positive <Display value={positivePercent}/>%</p>
+        </div>
+    )
+}
+
 
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    //variable that counts total feedback
-    let all = good + neutral + bad
-    //variable that counts average score (good: 1, neutral: 0, bad: -1)
-    let average = (good - bad) / all
-    //variable that calculates positive percentage from all feedback
-    let positivePercent = (((all - (neutral + bad)) / all) * 100)
+    let value = {
+        g: good,
+        n: neutral,
+        b: bad
+    }
 
     return (
         <div>
@@ -31,13 +52,7 @@ const App = () => {
             <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
             <Button handleClick={() => setBad(bad + 1)} text='bad'/>
             <br/>
-            <h1>statistics</h1>
-            <p>good <Display value={good}/></p>
-            <p>neutral <Display value={neutral}/></p>
-            <p>bad <Display value={bad}/></p>
-            <p>all <Display value={all}/></p>
-            <p>average <Display value={average}/></p>
-            <p>positive <Display value={positivePercent}/>%</p>
+            <Statistics {...value}/>
         </div>
     )
 }
