83cd75c961c9f3603014bf19b75ea1db5025f5f2
alekseyVY <alekseyvy@gmail.com>
2019-07-21 20:34:31 +0400

    v 0.8 part_1 (Exercises 1.7) unicafe part 2 (added all, average and positive)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,11 +10,18 @@ const Button = (props) => (
 
 const Display = (props) => <span>{props.value}</span>
 
+
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    //variable that counts total feedback
+    let all = good + neutral + bad
+    //variable that counts average score (good: 1, neutral: 0, bad: -1)
+    let average = (good - bad) / all
+    //variable that calculates positive percentage from all feedback
+    let positivePercent = (((all - (neutral + bad)) / all) * 100)
 
     return (
         <div>
@@ -28,7 +35,9 @@ const App = () => {
             <p>good <Display value={good}/></p>
             <p>neutral <Display value={neutral}/></p>
             <p>bad <Display value={bad}/></p>
-
+            <p>all <Display value={all}/></p>
+            <p>average <Display value={average}/></p>
+            <p>positive <Display value={positivePercent}/>%</p>
         </div>
     )
 }
