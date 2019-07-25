fbe52a69496365d834cddb21c81dd86ffbcf13a4
alekseyVY <alekseyvy@gmail.com>
2019-07-25 14:29:05 +0400

    part_2 (Exercises 2.2) added reduce function for getting sum of all exercises

--- part1/courseinfo/src/components/Course.js
+++ part1/courseinfo/src/components/Course.js
@@ -5,12 +5,14 @@ const Course = ({course}) => {
     const parts = course.parts
     const rows = (parts) => parts.map(part => <li key={part.id}>{part.name} : {part.exercises}</li>)
 
+    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
     return (
     <div>
         <h1>{course.name}</h1>
         <ul>
             {rows(parts)}
         </ul>
+        total of {total} exercises
     </div>
 
     )
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,45 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 
-/*const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.course.name}</h1>
-        </div>
-    )
-}
-
-const Part = (props) => {
-    return (
-        <div>
-            <h3>
-                {props.p} {props.e}
-            </h3>
-        </div>
-    )
-}
-
-const Content = (props) => {
-    eturn (
-        <>
-            <Part p = {props.course.parts[0].name} e = {props.course.parts[0].exercises} />
-            <Part p = {props.course.parts[1].name} e = {props.course.parts[1].exercises} />
-            <Part p = {props.course.parts[2].name} e = {props.course.parts[2].exercises} />
-        </>
-    )
-}
-
-const Total = (props) => {
-    return (
-        <div>
-            <h3>
-                Number of exercises: {props.course.parts[0].exercises +
-            props.course.parts[1].exercises + props.course.parts[2].exercises}
-            </h3>
-        </div>
-    )
-}*/
-
 
 
 
