6a406a9dd4a16e2220287faa3cb88043461e2cc1
alekseyVY <alekseyvy@gmail.com>
2019-07-26 21:06:29 +0400

    part_2 (Exercises 2.4, 2.5) added dynamic render of complex array of objects in array of objects, Course component separate module, I think logic will need refractor.

--- part1/courseinfo/src/components/Course.js
+++ part1/courseinfo/src/components/Course.js
@@ -1,20 +1,39 @@
 import React from 'react'
 
+// this is bruteforce approach I need to refractor this later :)
 
-const Course = ({course}) => {
-    const parts = course.parts
-    const rows = (parts) => parts.map(part => <li key={part.id}>{part.name} : {part.exercises}</li>)
-
-    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+const Course = ({courses}) => {
+    let parts = []
+    //iterates tru object and get name and parts into new array
+    courses.map((course, i) => {
+        parts.push(courses[i].name)
+        parts.push(course.parts)
+    })
+    let value = []
+    // using reduce method calculates total of rxercises
+    parts.map((v, i) => {
+        if (typeof v !== 'string') {
+            value.push(v.reduce((x, y) => x + y.exercises, 0))
+        }
+    })
+    let x = 0
+    //function that return rendering result of name and parts
+    const part = parts.map((part, i)=> {
+            if(typeof part === 'string') {
+                return <h2 key={i}>{part}</h2>
+            } else {
+                return ([
+                    part.map(x => <li key={x.id}>{x.name} : {x.exercises}</li>),
+                        <h3>total of {value[x++]} exercises</h3>
+                        ]
+            )
+    }
+})
     return (
-    <div>
-        <h1>{course.name}</h1>
-        <ul>
-            {rows(parts)}
-        </ul>
-        total of {total} exercises
-    </div>
-
+        <div>
+            <h1>Web development curriculum</h1>
+            {part}
+        </div>
     )
 }
 
