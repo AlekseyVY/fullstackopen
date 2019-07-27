2010a9652f9c942626b2aa29684132ed80e1a959
alekseyVY <alekseyvy@gmail.com>
2019-07-27 16:16:12 +0400

    part_2 (Exercises 2.8) added logic for submiting number with name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -11,14 +11,17 @@ const App =() => {
     const [persons, setPersons] = useState([
         {
             id: 1,
-            name: 'Arto Hellas'}
+            name: 'Arto Hellas',
+            number: '+70000000'
+        }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     let find = 0;
     const haveContact = () => {
         persons.map((p) => {
-            if (p.name === newName) {
+            if (p.name.toLowerCase() === newName.toLowerCase()) {
                 find = 1
             }
         })
@@ -28,22 +31,27 @@ const App =() => {
         event.preventDefault()
         const newContact = {
             id: persons.length + 1,
-            name: newName
+            name: newName,
+            number: newNumber
         }
         haveContact()
         if (find === 1){
-        window.alert(newName + ' is already added to phonebook')
+        window.alert(`${newName} is already added to phonebook`)
         } else {
             setPersons(persons.concat(newContact))
             setNewName('')
+            setNewNumber('')
         }
     }
 
-    const handleChange = (event) => {
+    const handleChangeName = (event) => {
         setNewName(event.target.value)
     }
+    const handleChangeNumber = (event) => {
+        setNewNumber(event.target.value)
+    }
 
-    const rows = (persons) => persons.map((p) => <li key={p.id}>{p.name}</li>)
+    const rows = (persons) => persons.map((p) => <li key={p.id}>name: {p.name} number: {p.number}</li>)
 
 
 
@@ -52,7 +60,10 @@ const App =() => {
             <h2>Phonebook</h2>
             <form onSubmit={addPhone}>
                 <div>
-                    name: <input  value={newName} onChange={handleChange}/>
+                    name: <input  value={newName} onChange={handleChangeName}/>
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={handleChangeNumber}/>
                 </div>
                 <div>
                     <button type='submit'>add</button>
